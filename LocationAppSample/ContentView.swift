1630672f08d09df6ad24e22f37fd6de351b4350e
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-01-22 23:43:31 +0900

    Initial Commit

---
+++ LocationAppSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationAppSample
+//
+//  Created by Fumiya Nakamura on 2020/01/22.
+//  Copyright © 2020 Fumiya Nakamura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
