872bbb22a812e51253085e5bf50a460b1ee08b4a
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-01-23 00:47:12 +0900

    Add MapView

---
+++ LocationAppSample/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  LocationAppSample
+//
+//  Created by Fumiya Nakamura on 2020/01/23.
+//  Copyright © 2020 Fumiya Nakamura. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(latitude: 37.3351, longitude: -122.0088)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
