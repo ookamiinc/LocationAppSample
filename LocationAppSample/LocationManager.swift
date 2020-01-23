64f01c3e87198008d3f1eff2bb309b8ad6aff85a
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-01-23 16:36:34 +0900

    Fix indent

--- LocationAppSample/LocationManager.swift
+++ LocationAppSample/LocationManager.swift
@@ -11,45 +11,45 @@ import Combine
 import CoreLocation
 
 class LocationManager: NSObject, ObservableObject {
-  private let locationManager = CLLocationManager()
-  private let geocoder = CLGeocoder()
-  let objectWillChange = PassthroughSubject<Void, Never>()
+    private let locationManager = CLLocationManager()
+    private let geocoder = CLGeocoder()
+    let objectWillChange = PassthroughSubject<Void, Never>()
 
-  @Published var status: CLAuthorizationStatus? {
-    willSet { objectWillChange.send() }
-  }
+    @Published var status: CLAuthorizationStatus? {
+        willSet { objectWillChange.send() }
+    }
 
-  @Published var location: CLLocation? {
-    willSet { objectWillChange.send() }
-  }
+    @Published var location: CLLocation? {
+        willSet { objectWillChange.send() }
+    }
 
-  @Published var placemark: CLPlacemark? {
-    willSet { objectWillChange.send() }
-  }
+    @Published var placemark: CLPlacemark? {
+        willSet { objectWillChange.send() }
+    }
 
-  override init() {
-    super.init()
+    override init() {
+        super.init()
 
-    self.locationManager.delegate = self
+        self.locationManager.delegate = self
 
-    self.locationManager.distanceFilter = kCLDistanceFilterNone
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    self.locationManager.activityType = .otherNavigation
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.activityType = .otherNavigation
 
-    self.locationManager.requestWhenInUseAuthorization()
-    self.locationManager.startUpdatingLocation()
-  }
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
 
-  private func geocode() {
-    guard let location = self.location else { return }
-    geocoder.reverseGeocodeLocation(location, completionHandler: { (places, error) in
-      if error == nil {
-        self.placemark = places?[0]
-      } else {
-        self.placemark = nil
-      }
-    })
-  }
+    private func geocode() {
+        guard let location = self.location else { return }
+        geocoder.reverseGeocodeLocation(location, completionHandler: { (places, error) in
+            if error == nil {
+                self.placemark = places?[0]
+            } else {
+                self.placemark = nil
+            }
+        })
+    }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
--- LocationAppSample/MotionManager.swift
+++ LocationAppSample/MotionManager.swift
@@ -11,26 +11,27 @@ import Combine
 import CoreMotion
 
 class MotionManager: NSObject, ObservableObject {
-  private let motionManager = CMMotionManager()
+    private let motionManager = CMMotionManager()
 
-  let objectWillChange = PassthroughSubject<Void, Never>()
+    let objectWillChange = PassthroughSubject<Void, Never>()
 
-  @Published var magnetometerData: CMMagnetometerData? {
-    willSet { objectWillChange.send()}
-  }
-  override init() {
-    super.init()
+    @Published var magnetometerData: CMMagnetometerData? {
+        willSet { objectWillChange.send()}
+    }
 
-    if self.motionManager.isMagnetometerAvailable {
-        self.motionManager.startMagnetometerUpdates(to: .main) { (magnetometerData, error) in
-            guard error == nil else {
-                print(error!)
-                return
-            }
-            if let magnetData = magnetometerData {
-                self.magnetometerData = magnetData
+    override init() {
+        super.init()
+
+        if self.motionManager.isMagnetometerAvailable {
+            self.motionManager.startMagnetometerUpdates(to: .main) { (magnetometerData, error) in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                if let magnetData = magnetometerData {
+                    self.magnetometerData = magnetData
+                }
             }
         }
     }
-  }
 }
