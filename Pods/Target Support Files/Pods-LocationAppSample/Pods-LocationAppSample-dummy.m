e90cd8f1297f9df2d259ada7507761dc58dbfbd1
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-02-05 13:32:59 +0900

    Run pod install

---
+++ Pods/Target Support Files/Pods-LocationAppSample/Pods-LocationAppSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LocationAppSample : NSObject
+@end
+@implementation PodsDummy_Pods_LocationAppSample
+@end
---
+++ Pods/Target Support Files/Pods-LocationAppSampleTests/Pods-LocationAppSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LocationAppSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_LocationAppSampleTests
+@end
