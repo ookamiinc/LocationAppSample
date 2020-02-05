58b04e085b6d104eabd7be04e6c71baeb0b584a5
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-02-05 16:42:48 +0900

    Send location to firebase

--- LocationAppSample/LocationManager.swift
+++ LocationAppSample/LocationManager.swift
@@ -9,11 +9,15 @@
 import Foundation
 import Combine
 import CoreLocation
+import FirebaseDatabase
 
 class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     private let geocoder = CLGeocoder()
     let objectWillChange = PassthroughSubject<Void, Never>()
+
+    var databaseRef: DatabaseReference!
+    var lastLocation: CLLocation?
 
     @Published var status: CLAuthorizationStatus? {
         willSet { objectWillChange.send() }
@@ -33,6 +37,8 @@ class LocationManager: NSObject, ObservableObject {
 
     override init() {
         super.init()
+
+        databaseRef = Database.database().reference()
 
         self.locationManager.delegate = self
 
@@ -56,6 +62,11 @@ class LocationManager: NSObject, ObservableObject {
             }
         })
     }
+
+    private func sendLocation(_ newLocation: CLLocation) {
+        let locationData = ["latitude": newLocation.latitude, "longitude": newLocation.longitude, "speed": newLocation.speed, "course": newLocation.course, "timestamp": newLocation.timestamp.timeIntervalSince1970.description, "timestamp_ms": (newLocation.timestamp.timeIntervalSince1970 * 1000).description, "date": newLocation.timestamp.description] as [String: Any]
+        databaseRef.child("Sample").childByAutoId().setValue(locationData)
+    }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
@@ -65,6 +76,11 @@ extension LocationManager: CLLocationManagerDelegate {
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
+        if let lastLocation = self.lastLocation {
+            guard lastLocation.timestamp != self.location?.timestamp else { return }
+            self.sendLocation(location)
+        }
+        self.lastLocation = self.location
         self.location = location
         self.geocode()
     }
