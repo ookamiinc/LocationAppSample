b0b8c633b0529d384c597d39bad67e9d4b1f5432
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2020-02-09 18:03:17 +0900

    Add signIn

--- LocationAppSample/AppDelegate.swift
+++ LocationAppSample/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Auth.auth().signIn(withEmail: "EMAIL", password: "PASSWORD") { authResult, error in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+        }
         return true
     }
 
